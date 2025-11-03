51ec643050557a055d8fc54621098628c7ea1762
kawtar-tns <tanassakawtar@gmai.com>
2025-11-03 18:52:00 +0100

    f

--- demo.jaxrs/src/main/java/ma/ws/jaxrs/JaxrsApplication.java
+++ demo.jaxrs/src/main/java/ma/ws/jaxrs/JaxrsApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @SpringBootApplication
@@ -19,9 +20,10 @@ public class JaxrsApplication{
 	@Bean
 	CommandLineRunner start(CompteRepository compteRepository){
 		return args -> {
-			compteRepository.save(new Compte(null, Math.random()*9000, new Date(), TypeCompte.EPARGNE));
-			compteRepository.save(new Compte(null, Math.random()*9000, new Date(), TypeCompte.COURANT));
-			compteRepository.save(new Compte(null, Math.random()*9000, new Date(), TypeCompte.EPARGNE));
+			compteRepository.save(new Compte(null, Math.random()*9000, LocalDate.now(), TypeCompte.EPARGNE));
+			compteRepository.save(new Compte(null, Math.random()*9000, LocalDate.now(), TypeCompte.COURANT));
+			compteRepository.save(new Compte(null, Math.random()*9000, LocalDate.now(), TypeCompte.EPARGNE));
+
 
 			compteRepository.findAll().forEach(c -> {
 				System.out.println(c.toString());
